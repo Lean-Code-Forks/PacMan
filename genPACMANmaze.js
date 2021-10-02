e617852caf5699573b8455e78ea1e2a5a95b212c
Neur0plasticity <41029495+Neur0plasticity@users.noreply.github.com>
2021-10-02 16:43:16 -0700

    Update genPACMANmaze.js

--- genPACMANmaze.js
+++ genPACMANmaze.js
@@ -13,15 +13,17 @@ class Spawn{ width = 40;  height = 40; }
 
 const gridWidth = 400 / 10, gridHeight = 500 / 10;
 
-const newRow = "\n", empty = " ", border = 0;
+const newRow = "\n", empty = " ", border = "0";
 
+InsertBordersIntoGrid:
 let str = ``;
 for (let row = 0; row < gridHeight; row++){
-    str += newRow;
     for (let col = 0; col < gridWidth; col++){
-        (col === 0) || (row === 0) && str += border
-        (col !== 0)                && str += empty;
+        if ( ((col === 0) || (row === 0)) && (str += border) ) {}
+        else if ( ((col === gridWidth-1) || (row === gridHeight-1)) && (str += border) ) {}
+        else if ( (col !== 0)                && (str += empty) ) {}
     }
+    str += newRow;
 }
 
 console.log(str);
